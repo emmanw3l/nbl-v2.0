11754d45034a4cdf7ff9d3fb41083eebe6d144b8
unknown <neonyinyechi@gmail.com>
2026-06-14 20:25:24 +0100

    please

--- client/src/components/dropdownBreadcrumb.tsx
+++ client/src/components/dropdownBreadcrumb.tsx
@@ -1,7 +1,7 @@
 // src/components/DropdownBreadcrumb.tsx
 import { useState, useEffect, useRef } from "react";
 import { Link, useNavigate }           from "react-router-dom";
-import "./DropdownBreadcrumb.css";
+import "./dropdownBreadcrumb.css";
 
 const API = import.meta.env.VITE_API_URL ?? "http://localhost:5000/api";
 
