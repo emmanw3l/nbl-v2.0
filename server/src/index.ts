d7deabd987d960e515c284d9c167aee1d858c864
unknown <neonyinyechi@gmail.com>
2026-07-01 20:43:40 +0100

    add

--- server/src/index.ts
+++ server/src/index.ts
@@ -51,7 +51,8 @@ const app  = express();
 const PORT = process.env.PORT ?? 5000;
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://nbl-v2-0.vercel.app"
+  "https://nbl-v2-0.vercel.app",
+  "https://nbl-v20-2vlhxfzdl-nwabueze-emmanuels-projects.vercel.app/"
 ];
 
 app.use(cors({
