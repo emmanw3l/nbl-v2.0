7059fa35c2a21d8df057bc19f75ba3b0a7cb6037
unknown <neonyinyechi@gmail.com>
2026-08-14 18:15:17 +0100

    take 3

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,36 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
 
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: "autoUpdate",
 
       manifest: {
-        name: 'Naija Book Lovers',
-        short_name: 'NBL',
-        description: 'A home for Nigerian poetry and literature',
-        theme_color: '#5f3205',
-        background_color: '#e2d7db',
-        display: 'standalone',
-        start_url: '/',
-        scope: '/',
+        name: "Naija Book Lovers",
+        short_name: "NBL",
+        description: "Naija Book Lovers",
+        start_url: "/",
+        scope: "/",
+        display: "standalone",
+
+        theme_color: "#5f3205",
+        background_color: "#e2d7db",
+
         icons: [
           {
-            src: '/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
           },
         ],
       },
     }),
   ],
-})
+});
